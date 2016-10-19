69f6b61fb0b79f46aa9141abf7267c9665638e63
root <root@aio1-utility-container-6c9d027a.openstack.local>
2016-10-19 16:18:44 +0000

    Fixed scaling issue.

--- heat_workload/workload_def.py
+++ heat_workload/workload_def.py
@@ -6,28 +6,31 @@ import fileinput
 import sys
 import requests
 import yaml
+
+
 @click.group()
 def workload_def():
     pass
 
-@workload_def.command('workload-create')
-@click.option('--name', type=str, required=True)
+
+@workload_def.command('workload-create', help = "Command to create workloads")
+@click.option('--name', type=str, required=True, help = "Desired workload name")
 @click.option('--insecure', required=False, default=True, type=str)
-@click.option('-n', required=False, default=1, type=int)
-def workload_define(name,insecure,n):
+@click.option('-n', required=False, default=1, type=int, help  = "Desired number of slices")
+def workload_define(name, insecure, n):
     print("Validating Quotas")
     validator = quota_validate(n)
-    if(validator == 1):
+    if (validator == 1):
         print("Quota Validated")
         template_path = os.path.abspath("/opt/ops-workload-framework/heat_workload/main.yaml")
         env_path = os.path.abspath("/opt/ops-workload-framework/heat_workload/envirnoment/heat_param.yaml")
         newline = "  \"num_of_slices\": " + str(n)
         pattern = "  \"num_of_slices\": "
-        replace(newline,pattern,env_path)
-        if(insecure == "True"):
-            comm = "openstack stack create -t " + template_path + " -e " + env_path + " "+ name + " --insecure"
+        replace(newline, pattern, env_path)
+        if (insecure == "True"):
+            comm = "openstack stack create -t " + template_path + " -e " + env_path + " " + name + " --insecure"
         else:
-            comm = "openstack stack create -t " + template_path + " -e " + env_path + " "+ name
+            comm = "openstack stack create -t " + template_path + " -e " + env_path + " " + name
         print(comm)
         print("Creating Stack...")
         os.system(comm)
@@ -37,11 +40,12 @@ def workload_define(name,insecure,n):
     else:
         print("Delete one of the resources that are full")
 
-@workload_def.command('scale-up')
+
+@workload_def.command('scale-up', help = "Scale up the workloads")
 @click.option('-sf', type=int, required=False, default=1, help="Adjust scaling factor")
 @click.option('--name', type=str, required=True, help="Name of the workload assigned during creation")
 @click.option('--insecure', required=False, default="True", type=str, help="Self signed certificate")
-def scale_up(sf,name, insecure):
+def scale_up(sf, name, insecure):
     print("Validating Quotas")
     validator = quota_validate(sf)
     if (validator == 1):
@@ -52,42 +56,63 @@ def scale_up(sf,name, insecure):
         comm_url = comm_1 + " | grep \"output_value: \" | awk 'BEGIN{ FS=\"output_value: \"}{print $2}' | awk 'BEGIN{ FS=\" \|\"}{print $1}'"
         url = subprocess.check_output(comm_url, shell=True)
         url = url.strip()
-        stream = open(env_path,'r')
-        data = yaml.load(stream)
-        sf = int(data['parameters']['num_of_slices']) + int(sf)
-        newline = "  \"num_of_slices\": " + str(sf)
-        pattern = "  \"num_of_slices\": "
-        replace(newline,pattern,env_path)
+        #stream = open(env_path, 'r')
+        #data = yaml.load(stream)
+        #sf = int(data['parameters']['num_of_slices']) + int(sf)
+        newline = "  \"scaling_size\": " + str(sf)
+        pattern = "  \"scaling_size\": "
+        replace(newline, pattern, env_path)
         if (insecure == "True"):
-            comm = "openstack stack update -t " + template_path + " -e " + env_path + " " + name + " --insecure" 
-            #for i in range(0, sf):
-        
-                #response = requests.post(url, verify=False)
-                #print response.status_code
-                #time.sleep(30)
-                #comm = "curl -XPOST --insecure -i " + "'" + url + "' &"
-                #os.system(comm)
-                #time.sleep(20)
+            comm = "openstack stack update -t " + template_path + " -e " + env_path + " " + name + " --insecure"
+            # for i in range(0, sf):
+            os.system(comm)
+            print("Update started...")
+            #response = requests.post(url, verify=False)
+            #print response.status_code
+            while True:
+                update_status = poll_update(name)
+                if update_status == 1:
+                  comm = "curl -XPOST --insecure -i " + "'" + url + "'"
+                  os.system(comm)
+                  print "Update Completed. Scaling requested.."
+                  break
+                elif update_status == -1:
+                  pass
+                elif update_status == 0:
+                  print "Update Failed."
+                  break
+
+            #time.sleep(20)
         else:
             comm = "openstack stack update -t " + template_path + " -e " + env_path + " " + name
-            #for i in range(0, sf):
-               # response = requests.post(url)
-               # print response.status_code
-               # time.sleep(30)
-                #comm = "curl -XPOST -i " + "'" + url + "'" + " &"
-                #os.system(comm)
-        #print(comm)
-        os.system(comm)
-        print("Scaling done...")
+            os.system(comm)
+            print("Update started...")
+            # for i in range(0, sf):
+            #response = requests.post(url)
+            #print response.status_code
+            while True:
+                update_status = poll_update(name)
+                if update_status == 1:
+                  comm = "curl -XPOST -i " + "'" + url + "'" + " &"
+                  os.system(comm)
+                  print "Update Completed. Scaling requested.."
+                  break
+                elif update_status == -1:
+                  pass
+                elif update_status == 0:
+                  print "Update Failed."
+                  break
+         # print(comm)
     elif validator == 0:
         print("Quota will exceed. Please reduce the scaling factor")
     else:
         print("Delete one of the resources that are full")
 
-@workload_def.command('check-status')
+
+@workload_def.command('check-status', help = "Check status of workload creation")
 @click.option('--name', type=str, required=True, help="Name of the workload assigned during creation")
 def check_status(name):
-    comm_1 =  "openstack stack show " + name
+    comm_1 = "openstack stack show " + name
     comm_status = comm_1 + "| grep \"CREATE_\""
     try:
         result = subprocess.check_output(comm_status, shell=True)
@@ -100,7 +125,22 @@ def check_status(name):
     except Exception, e:
         print("Exception")
 
-@workload_def.command('workload-delete')
+def poll_update(name):
+    comm_1 = "openstack stack show -f shell " + name
+    comm_status = comm_1 + "| grep  -w \"stack_status\" | cut -d \"=\" -f2"
+    try:
+        result = subprocess.check_output(comm_status, shell=True)
+        if "UPDATE_COMPLETE" in result:
+            return 1
+        elif "UPDATE_IN_PROGRESS" in result:
+            return -1
+        elif "UPDATE_FAILED" in result:
+            return 0
+    except Exception, e:
+        print ("Execution Failed. Please try again")
+
+
+@workload_def.command('workload-delete', help = "Delete workload and context")
 @click.option('--name', type=str, required=True, help="Name of the workload assigned during creation")
 def del_workload(name):
     comm_del = "openstack stack delete " + name
@@ -110,8 +150,9 @@ def del_workload(name):
     delete_context(env_path)
     print("Workload " + name + " deleted...")
 
+
 def delete_context(env_path):
-    stream = open(env_path,'r')
+    stream = open(env_path, 'r')
     data = yaml.load(stream)
     comm_del_flavor = "openstack flavor delete " + data['parameters']['instance_type']
     comm_del_image = "openstack image delete " + data['parameters']['image_id']
@@ -122,17 +163,18 @@ def delete_context(env_path):
     print("Context Deleted....")
 
 
-@workload_def.command('context-create')
+@workload_def.command('context-create', help= "Create Context")
 def create_context():
-   env_path=os.path.abspath("/opt/ops-workload-framework/heat_workload/envirnoment/heat_param.yaml")
-   flavor_name= create_flavor(env_path)
-   print("Flavor used to create workloads: " +flavor_name)
-   network_id= create_network(env_path)
-   print("Network used to create workloads: " + network_id)
-   image_id=create_image(env_path)
-   print("Image used to create workloads: " + image_id)
+    env_path = os.path.abspath("/opt/ops-workload-framework/heat_workload/envirnoment/heat_param.yaml")
+    flavor_name = create_flavor(env_path)
+    print("Flavor used to create workloads: " + flavor_name)
+    network_id = create_network(env_path)
+    print("Network used to create workloads: " + network_id)
+    image_id = create_image(env_path)
+    print("Image used to create workloads: " + image_id)
 
-@workload_def.command('quota-check')
+
+@workload_def.command('quota-check', help="Check quotas before creating workloads ")
 def quota_check():
     curr_instances = 0 if get_count("server") < 0 else get_count("server")
     curr_ports = 0 if get_count("port") < 0 else get_count("port")
@@ -141,7 +183,8 @@ def quota_check():
     curr_volumes = 1 * curr_instances
     curr_networks = 0 if get_count("network") < 0 else get_count("network")
     d = quota_parse()
-    print("Instances: \n" + "Current Usage: " + str(curr_instances) + "\n" + "Total Usage: " + str(d['instances']) + "\n")
+    print(
+    "Instances: \n" + "Current Usage: " + str(curr_instances) + "\n" + "Total Usage: " + str(d['instances']) + "\n")
     print("Ports: \n" + "Current Usage: " + str(curr_ports) + "\n" + "Total Usage: " + str(d['ports']) + "\n")
     print("Ram: \n" + "Current Usage: " + str(curr_ram) + "\n" + "Total Usage: " + str(d['ram']) + "\n")
     print("Cores: \n" + "Current Usage: " + str(curr_cores) + "\n" + "Total Usage: " + str(d['cores']) + "\n")
@@ -151,45 +194,47 @@ def quota_check():
 
 def create_flavor(env_path):
     comm = "openstack flavor create custom.workload.c1 --ram 2048 --disk 10 --vcpu 1 --public | awk 'BEGIN{ FS=\" id\"}{print $2}' | awk 'BEGIN{ FS=\" \"}{print$2}'"
-    #os.system("openstack flavor create m1.small_1 --ram 2048 --disk 10 --vcpu 1 --public")
+    # os.system("openstack flavor create m1.small_1 --ram 2048 --disk 10 --vcpu 1 --public")
     comm_check = "openstack flavor show custom.workload.c1"
     try:
-       result = subprocess.check_output(comm_check, shell=True)
-       if("No flavor with a name" in result):
-           flavor_id = subprocess.check_output(comm, shell=True)
-           flavor_id = flavor_id.strip()
-           newline = "  \"instance_type\": " + flavor_id
-           pattern = "  \"instance_type\": "
-           replace(newline,pattern,env_path)
-       else:
-           comm_check = "openstack flavor show custom.workload.c1 | awk 'BEGIN{ FS=\" id\"}{print $2}' | awk 'BEGIN{ FS=\" \"}{print$2}'"
-           flavor_id = subprocess.check_output(comm_check, shell=True)
-           print("Flavor custom.workload.c1 already present")
+        result = subprocess.check_output(comm_check, shell=True)
+        if ("No flavor with a name" in result):
+            flavor_id = subprocess.check_output(comm, shell=True)
+            flavor_id = flavor_id.strip()
+            newline = "  \"instance_type\": " + flavor_id
+            pattern = "  \"instance_type\": "
+            replace(newline, pattern, env_path)
+        else:
+            comm_check = "openstack flavor show custom.workload.c1 | awk 'BEGIN{ FS=\" id\"}{print $2}' | awk 'BEGIN{ FS=\" \"}{print$2}'"
+            flavor_id = subprocess.check_output(comm_check, shell=True)
+            print("Flavor custom.workload.c1 already present")
     except:
-       flavor_id = subprocess.check_output(comm, shell=True)
-       flavor_id = flavor_id.strip()
-       newline = "  \"instance_type\": " + flavor_id
-       pattern = "  \"instance_type\": "
-       replace(newline,pattern,env_path) 
+        flavor_id = subprocess.check_output(comm, shell=True)
+        flavor_id = flavor_id.strip()
+        newline = "  \"instance_type\": " + flavor_id
+        pattern = "  \"instance_type\": "
+        replace(newline, pattern, env_path)
     return flavor_id.strip()
 
-def replace(newline,pattern,env_path):
-    f= fileinput.input(env_path, inplace=True)
+
+def replace(newline, pattern, env_path):
+    f = fileinput.input(env_path, inplace=True)
     for line in f:
         if pattern in line:
-           line=newline+"\n"
+            line = newline + "\n"
         print(line),
     f.close()
 
+
 def create_network(env_path):
-    #os.system("neutron net-create net1")
-    print("Validate Network") 
+    # os.system("neutron net-create net1")
+    print("Validate Network")
     validator = validate_network()
-    if(validator == 1):
+    if (validator == 1):
         comm = "neutron net-create net1 | awk 'BEGIN{ FS=\" id\"}{print $2}' | awk 'BEGIN{ FS=\" \"}{print$2}'"
         net_id = subprocess.check_output(comm, shell=True)
         net_id = net_id.strip()
-        comm = "neutron subnet-create "+ net_id +" 192.168.2.0/24 --name subnet1"
+        comm = "neutron subnet-create " + net_id + " 192.168.2.0/24 --name subnet1"
         os.system(comm)
         newline = "  \"network_id\": " + net_id
         pattern = "  \"network_id\": "
@@ -199,6 +244,7 @@ def create_network(env_path):
     else:
         net_id = "Delete one of the networks"
     return net_id
+
 
 def create_image(env_path):
     comm = "openstack --os-image-api-version 1 image create centos_14.04 --location \"http://cloud.centos.org/centos/6/images/CentOS-6-x86_64-GenericCloud-1508.qcow2\" --disk-format qcow2 --container-format bare --public | awk 'BEGIN{ FS=\" id\"}{print $2}' | awk 'BEGIN{ FS=\" \"}{print$2}'"
@@ -214,8 +260,9 @@ def create_image(env_path):
         newline = "  \"image_id\": " + image_id
         pattern = "  \"image_id\": "
         replace(newline, pattern, env_path)
-    #os.system("openstack --os-image-api-version 1 image create ubuntu --location \"http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img\" --disk-format qcow2 --container-format bare --public")
+    # os.system("openstack --os-image-api-version 1 image create ubuntu --location \"http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img\" --disk-format qcow2 --container-format bare --public")
     return image_id.strip()
+
 
 def quota_parse():
     comm_instances = "openstack quota show -c instances admin -f shell  | cut -d \"=\" -f2"
@@ -224,13 +271,13 @@ def quota_parse():
     comm_volumes = "openstack quota show -c volumes admin -f shell  | cut -d \"=\" -f2"
     comm_networks = "openstack quota show admin -f shell | grep networ  | cut -d \"=\" -f2"
     comm_ports = "openstack quota show admin -f shell | grep port  | cut -d \"=\" -f2"
-    instances = int(subprocess.check_output(comm_instances, shell=True).strip().replace('"',''))
-    cores = int(subprocess.check_output(comm_cores, shell=True).strip().replace('"',''))
-    volumes = int(subprocess.check_output(comm_volumes, shell=True).strip().replace('"',''))
-    ram = int(subprocess.check_output(comm_ram, shell=True).strip().strip().replace('"',''))
-    networks = int(subprocess.check_output(comm_networks, shell=True).strip().replace('"',''))
-    ports = int(subprocess.check_output(comm_ports, shell=True).strip().replace('"',''))
-    d={}
+    instances = int(subprocess.check_output(comm_instances, shell=True).strip().replace('"', ''))
+    cores = int(subprocess.check_output(comm_cores, shell=True).strip().replace('"', ''))
+    volumes = int(subprocess.check_output(comm_volumes, shell=True).strip().replace('"', ''))
+    ram = int(subprocess.check_output(comm_ram, shell=True).strip().strip().replace('"', ''))
+    networks = int(subprocess.check_output(comm_networks, shell=True).strip().replace('"', ''))
+    ports = int(subprocess.check_output(comm_ports, shell=True).strip().replace('"', ''))
+    d = {}
     d['instances'] = sys.maxint if instances == -1 else instances
     d['cores'] = sys.maxint if cores == -1 else cores
     d['volumes'] = sys.maxint if volumes == -1 else volumes
@@ -239,27 +286,28 @@ def quota_parse():
     d['ports'] = sys.maxint if ports == -1 else ports
     return d
 
-def quota_validate(slice_num):
 
+def quota_validate(slice_num):
     curr_instances = get_count("server")
-  #  print(curr_instances)
+    #  print(curr_instances)
     curr_ports = get_count("port")
     curr_ram = 2048 * curr_instances
     curr_cores = 2 * curr_instances
     curr_volumes = 1 * curr_instances
     d = quota_parse()
-  #  print int(slice_num)
+    #  print int(slice_num)
     pred_instances = curr_instances + int(slice_num * 3)
-  #  print pred_instances
-    pred_ram = curr_ram + int( slice_num * 3 * 2048)
+    #  print pred_instances
+    pred_ram = curr_ram + int(slice_num * 3 * 2048)
     pred_cores = curr_cores + int(slice_num * 3 * 2)
     pred_volumes = curr_volumes + int(slice_num * 3 * 1)
- #   print pred_ram
-  #  print pred_cores
-  #  print pred_volumes
-  #  print d
+    #   print pred_ram
+    #  print pred_cores
+    #  print pred_volumes
+    #  print d
 
-    if((d['instances'] >= pred_instances and d['ram'] >= pred_ram and d['cores'] >= pred_cores and d['volumes'] >= pred_volumes)):
+    if ((d['instances'] >= pred_instances and d['ram'] >= pred_ram and d['cores'] >= pred_cores and d[
+        'volumes'] >= pred_volumes)):
         return 1
     else:
         if (d['instances'] < pred_instances):
@@ -272,18 +320,20 @@ def quota_validate(slice_num):
             print "Volume Quota will exceed: Predicted Usage: " + str(pred_volumes) + "/" + str(d['volumes'])
         return 0
 
+
 def get_count(str):
-    comm_1 = "openstack "+ str + " list"
+    comm_1 = "openstack " + str + " list"
     comm = comm_1 + " | wc -l"
     result = int(subprocess.check_output(comm, shell=True))
     result = result - 4
     return result
 
+
 def validate_network():
     curr_networks = get_count("network")
     d = quota_parse()
     pred_networks = curr_networks + 1
-    if(d['networks'] >= pred_networks):
+    if (d['networks'] >= pred_networks):
         return 1
     else:
         if (d['networks'] < pred_networks):
