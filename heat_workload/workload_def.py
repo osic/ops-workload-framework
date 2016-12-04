fcedccbdd76d66e3fce400e1f3acb61bdc9d9332
root <root@aio1.openstack.local>
2016-12-04 23:53:39 +0000

    - Added workloads for control plane - nova, neutron, swift

--- heat_workload/workload_def.py
+++ heat_workload/workload_def.py
@@ -202,7 +202,7 @@ def slice_add(name,add):
         for line in lines:
             i = i + 1
             count = len(line) - len(line.lstrip())
-            if i >= 31 and i<=88 and count > 2 and "get_param" in line:
+            if i >= 31 and i<=88 and (count ==6 or count ==8) and "get_param" in line:
                 if "- " in line: line = line.replace("  - ", "")
                 line = line.replace("\n", "")
                 block = block + line + "\n"
